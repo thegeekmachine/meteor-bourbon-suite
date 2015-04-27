ddbf35a198e6f5a9e0859f8ece82a04c8089e844
Shroff Amrith Nayak <shroff.nayak@flipkart.com>
2015-04-27 23:43:33 +0530

    Fix for jquery

--- refills/javascripts/refills/accordion.js
+++ refills/javascripts/refills/accordion.js
@@ -1,4 +1,4 @@
-$('.js-accordion-trigger').bind('click', function(e){
+jQuery('.js-accordion-trigger').bind('click', function(e){
   jQuery(this).parent().find('.submenu').slideToggle('fast');  // apply the toggle to the ul
   jQuery(this).parent().toggleClass('is-expanded');
   e.preventDefault();
